d980ae9e7fef28a6d937459c8b161a0539a37db2
EstherOkiri <est.okiri@gmail.com>
2024-03-09 13:16:34 +0300

    initial commit

---
+++ script.js
@@ -0,0 +1,8 @@
+//grab elements
+const playerChoice = document.getElementById('playerChoice');
+const computerChoice = document.getElementById('computerChoice');
+const result = document.getElementById('result');
+const choices = ['rock','paper', 'scissors'];
+
+//create an event listener over each button
+
