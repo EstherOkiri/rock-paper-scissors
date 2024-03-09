2165d1d167aeac093523345c9e7e27156bfe3ee6
EstherOkiri <est.okiri@gmail.com>
2024-03-09 16:59:55 +0300

    game logic added

--- script.js
+++ script.js
@@ -1,8 +1,47 @@
-//grab elements
-const playerChoice = document.getElementById('playerChoice');
-const computerChoice = document.getElementById('computerChoice');
-const result = document.getElementById('result');
-const choices = ['rock','paper', 'scissors'];
+const playerChoice= document.querySelector("#playerChoice");
+const computerChoice = document.querySelector("#computerChoice");
+const resultText = document.querySelector("#result");
+const choiceBtns = document.querySelectorAll(".choiceBtn");
+let player;
+let computer;
+let results;
+choiceBtns.forEach(button => button.addEventListener("click", () => {
 
-//create an event listener over each button
+    player = button.textContent;
+    computerTurn();
+    playerChoice.textContent = `Player: ${player}`;
+    computerChoice.textContent = `Computer: ${computer}`;
+    resultText.textContent = checkWinner();
+}));
 
+function computerTurn(){
+
+    const randNum = Math.floor(Math.random() * 3) + 1;
+
+    switch(randNum){
+      case 1:
+        computer = "Rock";
+        break;
+      case 2:
+        computer = "Paper";
+        break;
+      case 3:
+        computer = "Scissors";
+        break;
+    }
+}
+
+function checkWinner(){
+    if(player == computer){
+      return "Draw!";
+    }
+    else if(computer == "Rock"){
+      return (player == "Paper") ? "You Win!" : "You Lose!"
+    }
+    else if(computer == "Paper"){
+      return (player == "Scissors") ? "You Win!" : "You Lose!"
+    }
+    else if(computer == "Scissors"){
+      return (player == "Rock") ? "You Win!" : "You Lose!"
+    }
+}
